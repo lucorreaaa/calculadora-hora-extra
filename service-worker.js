da16ab7d5547f5ba8fb72ebfb09ecf323715ddd4
lucorreaaa <luca02699@hotmail.com>
2025-08-28 17:51:03 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'hora-extra-v10';
+const CACHE_NAME = 'hora-extra-v11';
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
@@ -61,4 +61,5 @@ self.addEventListener('fetch', event => {
     })
   );
 });
+
 
